515435da0acaf722e46810fc547cb4a98538c6df
programist78 <pavelmolodecfda@gmail.com>
2023-05-10 20:57:28 +0300

    op

--- app/components/HomeCom/Part1/index.jsx
+++ app/components/HomeCom/Part1/index.jsx
@@ -55,8 +55,8 @@ export default function Part () {
          {auth&& <button className={styles.change} onClick={handleClick}>Change✒️</button>} 
         {/* {data?.getLastVideo ?   <iframe width="560" height="315" src={`https://www.youtube.com/embed/${data?.getLastVideo}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.video}></iframe> : <div className={styles.video}></div>
         } */}
-        {data?.getLastVideo ?   <iframe width="560" height="315" src={`https://www.youtube.com/embed/zzZbcrhmYdU`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.video}></iframe> : <div className={styles.video}></div>
-}
+    <iframe width="560" height="315" src={`https://www.youtube.com/embed/zzZbcrhmYdU`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.video}></iframe> : <div className={styles.video}></div>
+
         </div>
     </div>
   )
